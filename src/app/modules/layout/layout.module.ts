e5102fb78dede459b0a8b485152feef8143c57c9
kennycallado <kennycallado@hotmail.com>
2023-03-18 21:53:56 +0100

    FIX Clean unused import and remove css class active

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './components/navigation/navigation.component';
 
