c57a2f39644e7d346b9c42e934ef2e8de22eab49
kennycallado <kennycallado@hotmail.com>
2023-03-18 16:26:21 +0100

    UPDATE Integra módlo layout en el root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LayoutModule } from './modules/layout/layout.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
