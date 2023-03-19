70666bf1b3184bf2cc19d85e91665f6bc77ec663
kennycallado <kennycallado@hotmail.com>
2023-03-19 16:17:29 +0100

    ADD Service to storage in localStorage

---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { UserInClaims } from '../providers/auth-user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  getUserData(): UserInClaims {
+    return JSON.parse(localStorage.getItem('user')!);
+  }
+
+  setUserData(data: UserInClaims) {
+    localStorage.setItem('user', JSON.stringify(data));
+  }
+
+  removeUserData(): void {
+    localStorage.removeItem('user');
+  }
+
+  getAccessToken(): string | null {
+    return localStorage.getItem('access_token');
+  }
+
+  setAccessToken(token: string) {
+    localStorage.setItem('access_token', token);
+  }
+
+  removeAccessToken(): void {
+    localStorage.removeItem('access_token');
+  }
+}
