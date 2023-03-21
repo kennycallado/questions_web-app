e620b8290076ce687f8183df9710879868a15e4e
kennycallado <kennycallado@hotmail.com>
2023-03-21 14:49:17 +0100

    ADD Agrega capacitor al proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.kenycallado.questions-app',
+  appName: 'questions-app',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
