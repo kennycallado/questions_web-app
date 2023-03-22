9ce1244f3d2913c2c954121f7b830678e15122a0
kennycallado <kennycallado@hotmail.com>
2023-03-22 10:47:54 +0100

    FIX Api urls por defecto

--- src/app/providers/constants.ts
+++ src/app/providers/constants.ts
@@ -1,4 +1,8 @@
 export const AUTH_API = 'https://questions.kennycallado.dev/auth';
+export const USER_API = 'https://questions.kennycallado.dev/api/v1/user';
+//
+// export const AUTH_API = 'http://localhost:8003/auth';
+// export const USER_API = 'http://localhost:8002/api/v1/user';
 
 // export const httpOptions = {
 //   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
