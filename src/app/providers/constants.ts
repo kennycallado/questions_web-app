20cb57aab88b61682868e49cedd895da4efa5099
kennycallado <kennycallado@hotmail.com>
2023-03-24 13:58:19 +0100

    ADD Agrega constante fcm_url

--- src/app/providers/constants.ts
+++ src/app/providers/constants.ts
@@ -1,5 +1,6 @@
 export const AUTH_API = 'https://questions.kennycallado.dev/auth';
 export const USER_API = 'https://questions.kennycallado.dev/api/v1/user';
+export const FCM_API  = 'https://questions.kennycallado.dev/api/v1/fcm';
 //
 // export const AUTH_API = 'http://localhost:8003/auth';
 // export const USER_API = 'http://localhost:8002/api/v1/user';
