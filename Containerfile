149c23ca2998d2001f1d0d9ca95f1c23882d3369
kennycallado <kennycallado@hotmail.com>
2023-03-24 13:09:24 +0100

    ADD Agrega autobuild ejecutando build.sh

--- Containerfile
+++ Containerfile
@@ -1,3 +1,38 @@
 FROM nginx:alpine
 
 COPY ./dist/ /usr/share/nginx/html
+COPY /nginx.conf  /etc/nginx/conf.d/default.conf
+EXPOSE 80
+
+### STAGE 1:BUILD ###
+# Defining a node image to be used as giving it an alias of "build"
+# Which version of Node image to use depends on project dependencies 
+# This is needed to build and compile our code 
+# while generating the docker image
+# Create a Virtual directory inside the docker image
+# Copy files to virtual directory
+# COPY package.json package-lock.json ./
+# Run command in Virtual directory
+# Copy files from local machine to virtual directory in docker image
+
+# FROM node:alpine AS build
+# WORKDIR /dist/src/app
+# RUN npm cache clean --force
+# COPY . .
+
+# ENV NG_CLI_ANALYTICS=ci
+
+# RUN npm install
+# RUN npm run build
+
+### STAGE 2:RUN ###
+# Defining nginx image to be used
+# Copying compiled code and nginx config to different folder
+# NOTE: This path may change according to your project's output folder 
+# Exposing a port, here it means that inside the container 
+# the app will be using Port 80 while running
+
+# FROM nginx:alpine AS ngi
+# COPY --from=build /dist/src/app/dist/my-docker-angular-app /usr/share/nginx/html
+# COPY /nginx.conf  /etc/nginx/conf.d/default.conf
+# EXPOSE 80
---
+++ build.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+
+platforms=("linux/amd64" "linux/arm64" "linux/arm/v7" "linux/arm/v6")
+package_name=$(jq -r '.name' package.json)
+package_version=$(jq -r '.version' package.json)
+
+# Build the project
+echo "Building the project..."
+npm install
+npm run build
+
+# Sync capacitor
+echo "Syncing capacitor..."
+npx cap sync
+
+# Build the android app
+echo "Building the android app..."
+# npx cap open android
+
+# Build the ios app
+echo "Building the ios app..."
+# npx cap open ios
+
+# Build the docker image
+echo "Building the docker images..."
+
+for platform in ${platforms[@]}; do
+  echo "Building docker image for: $platform."
+
+  tag=$(echo "${platform//\//_}" | tr -d 'linux_' | xargs -I {} echo {})
+
+  docker buildx build --no-cache --pull \
+    --platform $platform \
+    -t kennycallado/$package_name:${package_version}-${tag} \
+    -f ./Containerfile .
+done
+
+# Push the docker images
+echo "Pushing the docker images..."
+docker push -a kennycallado/${package_name}
+
+# Create the manifest
+echo "Creating the manifest for the version: $package_version"
+docker manifest create kennycallado/${package_name}:${package_version} \
+  kennycallado/${package_name}:${package_version}-amd64 \
+  kennycallado/${package_name}:${package_version}-arm64 \
+  kennycallado/${package_name}:${package_version}-armv7 \
+  kennycallado/${package_name}:${package_version}-armv6
+
+# manifest for latest version
+echo "Creating the manifest for latest version..."
+docker manifest create kennycallado/${package_name}:latest \
+  --amend kennycallado/${package_name}:${package_version}-amd64 \
+  --amend kennycallado/${package_name}:${package_version}-arm64 \
+  --amend kennycallado/${package_name}:${package_version}-armv7 \
+  --amend kennycallado/${package_name}:${package_version}-armv6
+
+
+# push the manifests
+echo "Pushing the manifests..."
+docker manifest push --purge kennycallado/${package_name}:${package_version}
+docker manifest push --purge kennycallado/${package_name}:latest
+
+# remove the images
+echo "Removing the images..."
+docker rmi kennycallado/${package_name}:${package_version}-amd64
+docker rmi kennycallado/${package_name}:${package_version}-arm64
+docker rmi kennycallado/${package_name}:${package_version}-armv7
+docker rmi kennycallado/${package_name}:${package_version}-armv6
+
+# remove the manifest
+echo "Cleaning up the manifest..."
+docker system prune -f
