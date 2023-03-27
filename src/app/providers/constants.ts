71497d0e3a913d002e99be9056de3dfd6a2358ed
kennycallado <kennycallado@hotmail.com>
2023-03-27 13:45:31 +0200

    UPDATE Manejo difernte direcciones de api para desarrollo

--- src/app/providers/constants.ts
+++ src/app/providers/constants.ts
@@ -2,8 +2,8 @@ export const AUTH_API = 'https://questions.kennycallado.dev/auth';
 export const USER_API = 'https://questions.kennycallado.dev/api/v1/user';
 export const FCM_API  = 'https://questions.kennycallado.dev/api/v1/fcm';
 //
-// export const AUTH_API = 'http://localhost:8003/auth';
-// export const USER_API = 'http://localhost:8002/api/v1/user';
+export const DEV_AUTH_API = 'http://localhost:8003/auth';
+export const DEV_USER_API = 'http://localhost:8002/api/v1/user';
 
 export const DEFAULT_PICTURE = 'https://picsum.photos/200/300';
 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,11 +1,11 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, isDevMode } from '@angular/core';
 
 import jwt_decode, { JwtPayload } from 'jwt-decode'
 import { Observable } from 'rxjs';
 
 import { AuthUser } from '../providers/auth-user';
-import { AUTH_API } from '../providers/constants';
+import { AUTH_API, DEV_AUTH_API } from '../providers/constants';
 
 import { StorageService } from './storage.service';
 
@@ -15,10 +15,15 @@ import { StorageService } from './storage.service';
 export class AuthService {
 
   status = new Observable<boolean>();
+  auth_api = AUTH_API;
 
   constructor(
     private http: HttpClient,
-    private storageSvc: StorageService) { }
+    private storageSvc: StorageService) {
+      if (isDevMode()) {
+        this.auth_api = DEV_AUTH_API;
+      }
+  }
 
   isAuthenticated(): boolean {
     let token = this.storageSvc.getAccessToken();
@@ -43,17 +48,17 @@ export class AuthService {
   }
 
   login(token: string): Observable<AuthUser> {
-    return this.http.post<AuthUser>(AUTH_API + '/login', token, { withCredentials: true });
+    return this.http.post<AuthUser>(this.auth_api + '/login', token, { withCredentials: true });
   }
 
   autoLogin(): Observable<AuthUser> {
-    return this.http.get<AuthUser>(AUTH_API + '/', { withCredentials: true });
+    return this.http.get<AuthUser>(this.auth_api + '/', { withCredentials: true });
   }
 
   logout(): void {
     // call logout api to invalidate token
     // Also call to remove the fcm token from the server
-    this.http.get(AUTH_API + '/logout', { withCredentials: true }).subscribe();
+    this.http.get(this.auth_api + '/logout', { withCredentials: true }).subscribe();
 
     this.storageSvc.removeUserData();
     this.storageSvc.removeAccessToken();
