7468b650d521c42327d3e857313028119896c172
kennycallado <kennycallado@hotmail.com>
2023-06-27 10:12:11 +0200

    UPDATE Actualiza constanters apis

--- src/app/providers/constants.ts
+++ src/app/providers/constants.ts
@@ -1,9 +1,9 @@
 export const AUTH_API = 'https://questions.kennycallado.dev/auth';
-export const USER_API = 'https://questions.kennycallado.dev/api/v1/user';
-export const FCM_API  = 'https://questions.kennycallado.dev/api/v1/fcm';
+export const USER_API = 'https://questions.kennycallado.dev/api/v1/user/';
+export const FCM_API  = 'https://questions.kennycallado.dev/api/v1/fcm/';
 //
-export const DEV_AUTH_API = 'http://localhost:8003/auth';
-export const DEV_USER_API = 'http://localhost:8002/api/v1/user';
+export const DEV_AUTH_API = 'http://localhost:8003/auth/';
+export const DEV_USER_API = 'http://localhost:8002/api/v1/user/';
 
 export const DEFAULT_PICTURE_URL = 'https://picsum.photos/200/300';
 
